b0b00014d5b58345acdf0fc59994d32489ad9689
Cadence Ember <cadence@disroot.org>
2020-11-25 13:34:08 +1300

    Proxy thumbnails in cloudtube

---
+++ api/thumbnails.js
@@ -0,0 +1,18 @@
+const fetch = require("node-fetch")
+const constants = require("../utils/constants.js")
+
+module.exports = [
+	{
+		route: `/vi/(${constants.regex.video_id})/(\\w+\\.jpg)`, methods: ["GET"], code: ({fill}) => {
+			const videoID = fill[0]
+			const file = fill[1]
+			return fetch(`https://i.ytimg.com/vi/${videoID}/${file}`).then(res => {
+				return {
+					statusCode: 200,
+					contentType: "image/jpeg",
+					stream: res.body
+				}
+			})
+		}
+	}
+]
