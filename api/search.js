0da13ac0e60c4fc7cbdbb559fd50e00346d84758
Cadence Ember <cadence@disroot.org>
2021-01-01 17:15:02 +1300

    Use preferred instance for search

--- api/search.js
+++ api/search.js
@@ -6,10 +6,10 @@ module.exports = [
 	{
 		route: "/(?:search|results)", methods: ["GET"], code: async ({req, url}) => {
 			const query = url.searchParams.get("q") || url.searchParams.get("search_query")
-			const fetchURL = new URL("http://localhost:3000/api/v1/search")
+			const instanceOrigin = getUser(req).getSettingsOrDefaults().instance
+			const fetchURL = new URL(`${instanceOrigin}/api/v1/search`)
 			fetchURL.searchParams.set("q", query)
 			const results = await fetch(fetchURL.toString()).then(res => res.json())
-			const instanceOrigin = getUser(req).getSettingsOrDefaults().instance
 			return render(200, "pug/search.pug", {query, results, instanceOrigin})
 		}
 	}
