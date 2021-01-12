11b9425cb0b1cf31dcadcc356ed8a7737fa00255
Cadence Ember <cadence@disroot.org>
2021-01-13 01:13:46 +1300

    Can't think?

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const {setInstance} = require("pinski/plugins")
 
 	server.addPugDir("pug", ["pug/includes"])
 	server.addRoute("/", "pug/home.pug", "pug")
+	server.addRoute("/cant-think", "pug/cant-think.pug", "pug")
 
 	server.addStaticHashTableDir("html/static/js")
 	server.addStaticHashTableDir("html/static/js/elemjs")
