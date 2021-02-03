d50e07f053c8bc3370b452e5e58aaefd2bbc5671
Cadence Ember <cadence@disroot.org>
2021-02-03 20:58:51 +1300

    Preserve other parameters in video redirection

--- api/redirects.js
+++ api/redirects.js
@@ -3,8 +3,10 @@ const {redirect} = require("pinski/plugins")
 
 module.exports = [
 	{
-		route: `/(${constants.regex.video_id})`, priority: -1, methods: ["GET"], code: async ({fill}) => {
-			return redirect(`/watch?v=${fill[0]}`, 301)
+		route: `/(${constants.regex.video_id})`, priority: -1, methods: ["GET"], code: async ({fill, url}) => {
+			const target = new URLSearchParams(url.search)
+			target.set("v", fill[0])
+			return redirect(`/watch?${target}`, 301)
 		}
 	}
 ]
