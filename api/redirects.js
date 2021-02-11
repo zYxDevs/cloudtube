e279fca60e3454ef986b463f3ea759c23dc2e60d
Cadence Ember <cadence@disroot.org>
2021-02-12 00:42:28 +1300

    Add comment explaining redirect method

--- api/redirects.js
+++ api/redirects.js
@@ -4,6 +4,16 @@ const {redirect} = require("pinski/plugins")
 module.exports = [
 	{
 		route: `/(${constants.regex.video_id})`, priority: -1, methods: ["GET"], code: async ({fill, url}) => {
+			/*
+			 Why not URLSearchParams?
+			 URLSearchParams is an unordered map, and URLs are more
+			 aesthetic if the video ID is at the start of them.
+			 This code makes the video ID always the first parameter, and
+			 then adds on the `search` from the original URL, with the
+			 leading ? replaced.
+			 If the original URL had no parameters, there will be no
+			 additional text added here.
+			*/
 			return redirect(`/watch?v=${fill[0]}${url.search.replace(/^\?/, "&")}`, 301)
 		}
 	}
