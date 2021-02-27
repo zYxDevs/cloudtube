a5668162238ad43d23cff4833e6331ea9ad66fe7
Cadence Ember <cadence@disroot.org>
2021-02-27 14:12:00 +1300

    Uncomment default instance block
    To force people to set it to point to their own instance.

--- config/config.sample.js
+++ config/config.sample.js
@@ -2,14 +2,12 @@ module.exports = {
 	/*
 	  Copy this file to `config.js`, and add options here.
 	  They'll override the options from `utils/constants.js`.
-	  For example, uncommenting the next block would change the default instance.
+	  For example, the next block changes the default instance.
 	*/
 
-	/*
 	user_settings: {
 		instance: {
 			default: "https://example.com"
 		}
 	}
-	*/
 }
