539260c8de5b7d0ad8ac7c8a025a0ff395b44245
Cadence Ember <cadence@disroot.org>
2021-08-19 19:33:47 +1200

    Improve documentation and errors around local mode

--- html/static/js/local-video.js
+++ html/static/js/local-video.js
@@ -1,18 +1,44 @@
-import {q} from "./elemjs/elemjs.js"
+const q = s => document.querySelector(s) // write this directly rather than importing to avoid an extra round-trip
 
-const status = q("#status")
+let status = q("#status")
 const form = q("#form")
 const data = q("#video-data")
 
+function displayError(root) {
+	let contents
+	if (root instanceof Error) { // error in our code here, or fetch API promise rejection
+		contents = root.toString()
+		if (root.stack) contents += root.stack
+	} else { // some JSON something
+		if (root.error) { // a descriptive report from the instance
+			contents = root.error.message || root.error
+			if (root.error.identifier) contents += `\nIdentifier: ${root.error.identifier}`
+		} else {
+			contents = JSON.stringify(root, null, 2)
+		}
+	}
+
+	console.log(contents)
+
+	const newStatus = document.createElement("pre")
+	newStatus.id = "status"
+	newStatus.textContent = contents
+	status.replaceWith(newStatus)
+	status = newStatus
+}
+
 fetch(`http://localhost:3000/api/v1/videos/${id}`).then(res => res.json()).then(root => {
-	if (root.error) throw new Error(root)
+	if (root.error) {
+		throw root // it's ok to throw this, it will be caught and displayed
+	}
+
 	data.value = JSON.stringify(root)
 	form.submit()
 	status.textContent = "Submitting..."
 }).catch(e => {
-	if (e.message.includes("NetworkError")) {
+	if (e.message && e.message.includes("NetworkError")) {
 		status.textContent = "Connection failed. Make sure you're running your own instance locally."
 	} else {
-		status.innerText = e.toString()
+		displayError(e)
 	}
 })
